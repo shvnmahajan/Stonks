c7700478db87a012de642467a7af8adee01b3936
Siddhant Deshmukh <siddhantdeshmukh99@gmail.com>
2020-04-13 17:15:36 +0530

    stage 0

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask,render_template
+app=Flask(__name__,template_folder='template')
+@app.route("/")
+@app.route("/index")
+def about():
+	return render_template("about.html")
+
+@app.route("/recomendation")
+def education():
+	return render_template("recomendation.html",recomendations=recomendations);
+
+if __name__=="__main__":
+	app.run(debug=True)
