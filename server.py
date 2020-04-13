b388b2a7468591a0b34d6aed37bc82710bc2f924
shvnmahajan <shravanmahajan123@gmail.com>
2020-04-13 19:16:34 +0530

    Recommendation Updates

--- server.py
+++ server.py
@@ -1,13 +1,32 @@
 from flask import Flask,render_template
 app=Flask(__name__,template_folder='template')
+
 @app.route("/")
-@app.route("/index")
+@app.route("/about")
 def about():
 	return render_template("about.html")
 
+recomendations = [
+	{
+		'call' : 'buy',
+		'name' : 'reliance'	
+	},
+
+	{
+		'call' : 'buy',
+		'name' : 'sid'	
+	},
+
+	{
+		'call' : 'sell',
+		'name' : 'mid'	
+	}
+]
+
+
 @app.route("/recomendation")
 def recomendation():
-	return render_template("recomendation.html",recomendations=recomendations);
+	return render_template("recomendation.html",recos=recomendations);
 
 if __name__=="__main__":
 	app.run(debug=True)
